5ffdd259eba08efaec637cc2552b5e180666e5af
scmuhm <130038735+scmuhm@users.noreply.github.com>
2024-06-14 15:23:23 +0800

    Update PdfTestComp.tsx

--- src/sxastarter/src/components/PdfTestComp.tsx
+++ src/sxastarter/src/components/PdfTestComp.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 export const Default = (): JSX.Element => {
   return (
     <div>
-      <h1>Test PDF Viewer</h1>
-      <iframe
+      <h1>Test PDF Viewer</h1>    
+      <object
         className="absolute top-0 left-0 w-full h-full"
-        src="https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf"
-        width="100%"
-        height="100%"
-        title="test PDF"
-      ></iframe>
+        data="https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf"
+        title="testpdf"
+      ></object>
     </div>
   );
 };
