5855fdbc7196f7d13de1b5171c62584d62f18415
scmuhm <130038735+scmuhm@users.noreply.github.com>
2024-06-14 15:00:05 +0800

    Update PdfTestComp.tsx

--- src/sxastarter/src/components/PdfTestComp.tsx
+++ src/sxastarter/src/components/PdfTestComp.tsx
@@ -6,7 +6,7 @@ export const Default = (): JSX.Element => {
       <h1>Test PDF Viewer</h1>
       <iframe
         className="absolute top-0 left-0 w-full h-full"
-        src='https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf'
+        src="https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf"
         width="100%"
         height="100%"
         title="test PDF"
