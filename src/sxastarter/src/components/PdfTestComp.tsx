73baafb703f6c6460d6d1281b3d2bbfaf10c7488
scmuhm <130038735+scmuhm@users.noreply.github.com>
2024-06-18 11:54:36 +0800

    Update PdfTestComp.tsx

--- src/sxastarter/src/components/PdfTestComp.tsx
+++ src/sxastarter/src/components/PdfTestComp.tsx
@@ -4,11 +4,19 @@ export const Default = (): JSX.Element => {
   return (
     <div>
       <h1>Test PDF Viewer</h1>    
-      <object
+      <iframe
         className="absolute top-0 left-0 w-full h-full"
-        data="https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf"
+        src="https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf#toolbar=0"
+        width="100%"
+        height="100%"
         title="testpdf"
-      ></object>
+      ></iframe>    
     </div>
   );
 };
+
+//<object
+//        className="absolute top-0 left-0 w-full h-full"
+//        data="https://xmc-pssapj1-muhmlatestxmc-dev.sitecorecloud.io/-/media/Project/testSite/testSite/ADV_Plate.pdf"
+//        title="testpdf"
+//      ></object> /*
