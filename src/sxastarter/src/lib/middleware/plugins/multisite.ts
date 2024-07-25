33ac24ffaab9cd2139c3b3aa5aec924152851bca
scmuhm <130038735+scmuhm@users.noreply.github.com>
2024-07-25 15:01:18 +0800

    cs0456075 customization

--- src/sxastarter/src/lib/middleware/plugins/multisite.ts
+++ src/sxastarter/src/lib/middleware/plugins/multisite.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { MultisiteMiddleware } from '@sitecore-jss/sitecore-jss-nextjs/middleware';
+import { MultisiteMiddleware, debug } from '@sitecore-jss/sitecore-jss-nextjs/middleware';
 import { siteResolver } from 'lib/site-resolver';
 import { MiddlewarePlugin } from '..';
+import { SitesMap } from 'scripts/graphql-extended-siteinfo-service';
+import JssConfig from '../../../temp/config';
+import { extractPathComponents } from 'src/utils';
 
 /**
  * This is the multisite middleware plugin for Next.js.
@@ -32,6 +35,74 @@ class MultisitePlugin implements MiddlewarePlugin {
   }
 
   async exec(req: NextRequest, res?: NextResponse): Promise<NextResponse> {
+    if (req.nextUrl.pathname !== req.nextUrl.pathname.toLowerCase()) {
+      return NextResponse.redirect(
+        new URL(
+          `${req.nextUrl.origin}${req.nextUrl.pathname.toLowerCase()}?${req.nextUrl.searchParams}`
+        ),
+        301 // Set Permanent Redirect
+      );
+    }
+    debug.multisite('MultisitePlugin - origin: %o', req.headers.get('origin'));
+    debug.multisite('MultisitePlugin - hostname: %o', req.headers.get('hostname'));
+    debug.multisite('MultisitePlugin - host: %o', req.headers.get('host'));
+    debug.multisite('MultisitePlugin - Origin: %o', req.headers.get('Origin'));
+    debug.multisite('MultisitePlugin - Hostname: %o', req.headers.get('Hostname'));
+    debug.multisite('MultisitePlugin - Host: %o', req.headers.get('Host'));
+    debug.multisite('MultisitePlugin - pathName: %o', req.nextUrl.pathname.toLowerCase());
+    // BEGIN: Nextjs multisite routing customisations based on virtual folder
+    const requestHeaders = new Headers(req.headers);
+    const hostname = req.headers.get('host')?.replace(/:\d+$/, '') ?? '';
+
+    let sitesMap: SitesMap = {};
+    const multisitesData: { [key: string]: SitesMap } = JssConfig.sites
+      ? JSON.parse(JssConfig.sites)
+      : {};
+    debug.multisite('MultisitePlugin - multisitesData: %o', multisitesData);
+    if (Object.keys(multisitesData).length > 0) {
+      sitesMap = multisitesData[hostname];
+    }
+    debug.multisite('MultisitePlugin - sitesMap: %o', sitesMap);
+
+    // extract country code, locale and rest of the path from the url path
+    const { countrycode, languagecode, pathname } = extractPathComponents(
+      hostname,
+      req.nextUrl.pathname
+    );
+
+    debug.multisite('MultisitePlugin - extractPathComponents: %o', {
+      countrycode,
+      languagecode,
+      pathname,
+    });
+
+    // set sc_site cookie with the country code in the url path
+    // The value of custome header is set to resolve based on siteName. Search for instances of `siteResolver.getByName`
+    if (countrycode) {
+      req.cookies.set('sc_site', sitesMap[countrycode]);
+      requestHeaders.set('x-sc-site', sitesMap[countrycode]);
+    } else {
+      req.cookies.set('sc_site', sitesMap['']);
+      requestHeaders.set('x-sc-site', sitesMap['']);
+    }
+
+    debug.multisite('MultisitePlugin - sc_site: %o', req.cookies.get('sc_site'));
+
+    // set the req locale and path with the ones extracted from the url path
+    req.nextUrl.locale = languagecode as string;
+    req.nextUrl.pathname = pathname;
+
+    // set request headers in NextResponse.rewrite
+    const response = NextResponse.next({
+      request: {
+        // New request headers
+        headers: requestHeaders,
+      },
+    });
+
+    res = response || res;
+    // END: Nextjs multisite routing customisations based on virtual folder
+
     return this.multisiteMiddleware.getHandler()(req, res);
   }
 }
